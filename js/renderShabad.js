be051c174f6ed64b4bd6c759ce1cfdf84ed8cc8c
Divjot Singh <bogas04@gmail.com>
2016-12-19 22:49:11 +0530

    fix #9 #15 #16 #18

--- js/renderShabad.js
+++ js/renderShabad.js
@@ -1,5 +1,9 @@
 function renderShabad(gurbani) {
-  const baani = h('div', { class: 'shabad-content' } , gurbani.map(({ shabad }) => h('div', {  }, [
+  $shabad.appendChild(h('div', { class: 'shabad-container' }, [ buttons(), baani(gurbani), ]));
+}
+
+function baani(gurbani) {
+  return h('div', { class: 'shabad-content' } , gurbani.map(({ shabad }) => h('div', {  }, [
     h('p', { class: 'gurlipi gurbani-font' }, shabad.Gurmukhi),
     h('p', { class: 'unicode gurbani-font hidden' }, shabad.GurmukhiUni),
     h('p', { class: 'transliteration english hidden' }, shabad.Transliteration),
@@ -7,47 +11,51 @@ function renderShabad(gurbani) {
     h('blockquote', { class: 'translation english hidden' }, shabad.English),
     h('blockquote', { class: 'translation spanish hidden' }, shabad.Spanish),
   ])));
-
-  const buttons = h('div', { class: 'buttons flex wrap flex-start align-center no-select' }, [
-    renderCheckbox({
-      id: 'unicode-toggle',
-      text: 'Unicode',
-      click: e => {
-        [...$shabad.querySelectorAll('.unicode')].forEach(el => el.classList.toggle('hidden'));
-        [...$shabad.querySelectorAll('.gurlipi')].forEach(el => el.classList.toggle('hidden'));
-      },
-    }),
-    renderCheckbox({
-      id: 'transliteration.english-toggle',
-      text: 'English Transliteration',
-      click: e => [...$shabad.querySelectorAll('.transliteration.english')].forEach(el => el.classList.toggle('hidden'))
-    }),
-    renderCheckbox({
-      id: 'translation.punjabi-toggle',
-      text: 'Punjabi Translation',
-      click: e => [...$shabad.querySelectorAll('.translation.punjabi')].forEach(el => el.classList.toggle('hidden'))
-    }),
-    renderCheckbox({
-      id: 'translation.english-toggle',
-      text: 'English Translation',
-      click: e => [...$shabad.querySelectorAll('.translation.english')].forEach(el => el.classList.toggle('hidden'))
-    }),
-    renderCheckbox({
-      id: 'translation.spanish-toggle',
-      text: 'Spanish Translation',
-      click: e => [...$shabad.querySelectorAll('.translation.spanish')].forEach(el => el.classList.toggle('hidden'))
-    }),
-  ]);
-
-  $shabad.appendChild(h('div', { class: 'shabad-container' }, [
-    buttons,
-    baani,
-  ]));
 }
 
-function renderCheckbox({ id, text, click }) {
-  return h('div', {  }, [
-    h('input', { id, type: 'checkbox', click, class: 'no-select' }),
-    h('label', { 'for': id, class: 'no-select' }, text),
+function buttons () {
+  const transliterations = {
+    'Transliteration': null,
+    'English Transliteration': '.transliteration.english'
+  };
+
+  const translations = {
+    'Translation': null,
+    'Punjabi Translation': '.translation.punjabi',
+    'English Translation': '.translation.english',
+    'Spanish Translation': '.translation.spanish',
+  };
+
+  function renderCheckbox({ id, text }) {
+    function click () {
+      Array.from($shabad.querySelectorAll('.unicode')).forEach(el => el.classList.toggle('hidden'));
+      Array.from($shabad.querySelectorAll('.gurlipi')).forEach(el => el.classList.toggle('hidden'));
+    }
+    return h('div', { }, [
+      h('input', { id, type: 'checkbox', click, class: 'no-select' }),
+      h('label', { 'for': id, class: 'no-select' }, text),
+    ]);
+  }
+
+  function renderDropdown({ name, data }) {
+    function change({ target: { value, options } }) {
+      Array.from(options)
+        .map(e => Array.from($shabad.querySelectorAll(e.value)))
+        .forEach(els => els.forEach(el => el.classList.add('hidden')));
+
+      if (value)
+        Array.from($shabad.querySelectorAll(value))
+          .forEach(el => el && el.classList.remove('hidden'));
+    }
+    return h('div', { }, [
+      h('select', { name, change, }, Object.keys(data).map(key => h('option', { value: data[key] }, key)))
+    ]);
+  }
+
+  return h('div', { class: 'buttons flex wrap justify-start align-start no-select' }, [
+    renderCheckbox({ id: 'unicode-toggle', text: 'Unicode', }),
+    renderDropdown({ name: 'transliterations', data: transliterations }),
+    renderDropdown({ name: 'translations', data: translations }),
   ]);
 }
+
