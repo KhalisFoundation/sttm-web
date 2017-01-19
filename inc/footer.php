855f6a453a859db410d1f3f0b287c31767afe60d
Navdeep Singh <me@navdeepsingh.ca>
2017-01-19 09:58:30 -0800

    URL versioning for JS and CSS files

--- inc/footer.php
+++ inc/footer.php
@@ -11,15 +11,15 @@
 
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.2.4/foundation.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="js/main.js?v=<?= $asset_version ?>"></script>
     <script src="js/h.js"></script>
     <script src="js/getParameterByName.js"></script>
-    <script src="js/khajana.js"></script>
+    <script src="js/khajana.js?v=<?= $asset_version ?>"></script>
 
 <?php
   if (is_array($scripts)) {
     foreach ($scripts as $script) {
-      echo "    <script src=\"{$script}\"></script>\n";
+      echo "    <script src=\"{$script}?v=<?= $asset_version ?>\"></script>\n";
     }
   }
 ?>
--- inc/head.php
+++ inc/head.php
@@ -6,6 +6,6 @@
     <title><?= $title ? $title . ' - ' : '' ?>SikhiToTheMax</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.2.4/foundation.min.css" />
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=<?= $asset_version ?>">
   </head>
   <body class="<?= @implode(' ', $body_classes) ?>">
