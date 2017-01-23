c567fee174c48539d17b38cee169cebb9395d058
Navdeep Singh <me@navdeepsingh.ca>
2017-01-23 00:57:17 -0800

    About page + Footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,11 @@
     </div>
+    <footer>
+      <div class="row">
+        <ul class="menu">
+          <li><a href="/about">About</a></li>
+        </ul>
+      </div>
+    </footer>
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
