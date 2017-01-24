5344aed0751f9d4936f69cdfa64c44500589fb94
Navdeep Singh <me@navdeepsingh.ca>
2017-01-24 10:10:45 -0800

    Messaging to indicate from where Hukamnama is

--- pages/hukamnama.php
+++ pages/hukamnama.php
@@ -2,9 +2,14 @@
 
 $scripts[] = 'js/hukamnama.js';
 $scripts[] = 'js/renderShabad.js';
-require_once('inc/controls.php');
 
 ?>
+    <div class="body_text">
+      <h3 style="text-align: center;">Daily Hukamnama from Sri Harmandir Sahib, Amritsar</h3>
+    </div>
+
+<?php require_once('inc/controls.php'); ?>
+
     <div>
       <div id="shabad" class="shabad translation-english">
       </div>
