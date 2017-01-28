7594ff39c43811916a42641e0e3c45d1d7663c23
Navdeep Singh <me@navdeepsingh.ca>
2017-01-27 21:30:50 -0800

    Give more font formats the right to self-determination when served via dev PHP server

--- router.php
+++ router.php
@@ -1,5 +1,5 @@
 <?php
-if (preg_match('/\.(?:png|jpg|jpeg|gif|win32|css|js|ttf|ico)/', $_SERVER["REQUEST_URI"])) {
+if (preg_match('/\.(?:png|jpg|jpeg|gif|win32|css|js|ttf|ico|eot|svg|woff|woff2|otf)/', $_SERVER["REQUEST_URI"])) {
     return false;
 } else {
     include __DIR__ . '/index.php';
