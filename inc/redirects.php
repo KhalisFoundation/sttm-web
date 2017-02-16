76072c42475d82bd4246be2e2c45f2bbb53889b9
Navdeep Singh <me@navdeepsingh.ca>
2017-02-16 14:42:42 -0800

    Fix for redirects Add ShabadID redirect from previous URL structure

--- inc/redirects.php
+++ inc/redirects.php
@@ -1,10 +1,15 @@
 <?php
 
-if ($_SERVER['SCRIPT_NAME'] == "/page.asp") {
+if ($_SERVER['REDIRECT_URL'] == "/page.asp") {
   parse_str($_SERVER['QUERY_STRING'], $query_string);
   if ($query_string['SourceID'] && $query_string['PageNo']) {
+    $redirect_url = "/ang?ang={$query_string['PageNo']}&source={$query_string['SourceID']}";
+  } else if ($query_string['ShabadID']) {
+    $redirect_url = "/shabad?id={$query_string['ShabadID']}";
+  }
+  if ($redirect_url) {
     header("HTTP/1.1 301 Moved Permanently");
-    header("location: /ang?ang={$query_string['PageNo']}&source={$query_string['SourceID']}");
+    header("location: $redirect_url");
     exit;
   }
 }
