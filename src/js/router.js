607ee5eebff6bb6f9b0d6547359feb5484ef45ed
Divjot Singh <bogas04@gmail.com>
2017-09-25 14:16:43 +0530

    Update router.js

--- src/js/router.js
+++ src/js/router.js
@@ -82,7 +82,7 @@ const routes = {
             <h1 id="error-code">404</h1>
             <div id="error-msg">These are not the Singhs you are looking for.</div>
             <div id="error-desc">
-              The requested URL <code>${getQueryParams().fromh.split('?')}</code> was not found on this server.
+              The requested URL <code>${getQueryParams().from}</code> was not found on this server.
             </div>
           </div>
           <div class="small-12 medium-5 columns">
