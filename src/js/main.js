b4610c330c8f989a11f39614ba61e5d88c952365
Divjot Singh <bogas04@gmail.com>
2018-01-10 08:53:43 +0530

    Split View Feature (fix #87) (#191)
    * Split View Feature (fix #87)
    
    * Add Split View Feature
    * Code cleanup
    * Transform renderControls to JSX
    * Fix a bug where shared panktee was undefined
    * Update webpack, node-sass
    
    * Remove some more jQuery usage, use classList.toggle
    
    * Add null checks

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,53 @@
-const $search       = document.getElementById("search");
-const $searchType   = document.getElementById("search-type");
-let   $controls     = document.getElementById("controls-wrapper");
-let   $shabad       = document.getElementById("shabad");
-let   $meta         = document.getElementById("metadata");
-const prefs         = {};
-const default_prefs = {
-  'displayOptions': ['translation-english', 'transliteration-english'],
-  'shabadToggles': []
+const $search = document.getElementById('search');
+const $searchType = document.getElementById('search-type');
+let   $controls = document.getElementById('controls-wrapper');
+let   $shabad = document.getElementById('shabad');
+let   $meta = document.getElementById('metadata');
+const prefs = {};
+const defaultPrefs = {
+  displayOptions: ['translation-english', 'transliteration-english'],
+  shabadToggles: [],
 };
+
+function shortenURL(url = window.location.href) {
+  const path = window.location.pathname;
+  const shortdomain = 'sttm.ws';
+  const URL = `http://${shortdomain}`;
+
+  switch (path) {
+    case '/shabad': return `${URL}/s/${getParameterByName('id')}`;
+    case '/ang': return `${URL}/a/${getParameterByName('ang')}`;
+    case '/hukamnama': return `${URL}/h`;
+    default: return url.replace(window.location.hostname, shortdomain);
+  }
+}
+
+const getPrefs = () => Object
+  .keys(defaultPrefs)
+  .forEach(key => prefs[key] = localStorage.getItem(key)
+    ? JSON.parse(localStorage.getItem(key))
+    : defaultPrefs[key]
+  );
+
+const setPref = (key, val) => localStorage.setItem(key, JSON.stringify(val));
+
+function checkboxPref(e, key, option) {
+  if (e.target.classList.contains('active')) {
+    if (prefs[key].indexOf(option) < 0) {
+      prefs[key].push(option);
+    }
+  } else {
+    const index = prefs[key].indexOf(option);
+    prefs[key].splice(index, 1);
+  }
+  setPref(key, prefs[key]);
+}
 
 getPrefs();
 
-if ($searchType) $searchType.addEventListener("change", updateSearchLang);
-if ($searchType) $searchType.addEventListener("change", updateSearchAction);
+if ($searchType) $searchType.addEventListener('change', updateSearchLang);
+
+if ($searchType) $searchType.addEventListener('change', updateSearchAction);
 
 $search.onkeyup = function () {
   if ($searchType.value == 5 && this.value != this.value.replace(/[^0-9]/g, '')) {
@@ -20,58 +55,60 @@ $search.onkeyup = function () {
   }
 };
 
-$(".search-form").on("submit", function(e) {
-  if ($search.value.length <= 2 && $searchType.value != 5) {
-    alert("Please enter at least 3 characters");
-    e.preventDefault();
-    return false;
-  }
-});
+[document.querySelector('.search-form')]
+  .forEach(f => f && f.addEventListener('submit', e => {
+    if ($search.value.length <= 2 && $searchType.value != 5) {
+      alert('Please enter at least 3 characters');
+      e.preventDefault();
+      return false;
+    }
+  }));
 
-//Shabad controller toggles
-$(".shabad-controller-toggle").on("click", shabadToggle);
+// Shabad controller toggles
+[...document.querySelectorAll('.shabad-controller-toggle')]
+  .forEach(e => e && e.addEventListener('click', shabadToggle));
 
-//Shabad display option toggles
-$(".display-option-toggle").on("click", displayOptionToggle);
+// Shabad display option toggles
+[...document.querySelectorAll('.display-option-toggle')]
+  .forEach(e => e && e.addEventListener('click', displayOptionToggle));
 
-$("#open-mobile-menu").on("click", function() {
-  document.body.classList.toggle("menu-open");
-})
-$(".top-bar-right .close a").on("click", function() {
-  document.body.classList.remove("menu-open");
-})
+[document.getElementById('open-mobile-menu')]
+  .forEach(e => e && addEventListener('click', () => document.body.classList.toggle('menu-open')));
 
-$("#open_share_menu").on("click", function() {
-  document.body.classList.toggle("share-open");
-})
+[document.querySelector('.top-bar-right .close a')]
+  .forEach(e => e && addEventListener('click', () => document.body.classList.remove('menu-open')));
 
-$("#search-options select").on("change", function() {
-  var update = $(this).data("update");
-  $("#" + update).val($(this).val());
-  if ($(this).attr("id") == "search-source") {
-    $("#top-bar-search-form").submit();
-  }
-});
+[document.getElementById('open_share_menu')]
+  .forEach(e => e && addEventListener('click', () => document.body.classList.toggle('share-open')));
 
-document.querySelector(".gurmukhi-keyboard-toggle").addEventListener("click", function() {
-  document.querySelector('.gurmukhi-keyboard').classList.toggle('active');
-});
+[...document.querySelectorAll('#search-options select')]
+  .forEach(e => e && e.addEventListener('change', function () {
+    const update = this.dataset.update;
+    document.getElementById(update).value = this.value;
+    if (this.getAttribute('id') === 'search-source') {
+      document.getElementById('top-bar-search-form').submit();
+    }
+  }));
 
-$("#shabad").on("click", ".share .copy", function() {
-  let el = $(this).parents(".line").children("textarea");
+[document.querySelector('.gurmukhi-keyboard-toggle')]
+  .forEach(e => e && e.addEventListener('click', () => document.querySelector('.gurmukhi-keyboard').classList.toggle('active')));
+
+$('#shabad').on('click', '.share .copy', function() {
+  const el = $(this).parents('.line').children('textarea');
   el.show().select();
-  document.execCommand("copy");
+  document.execCommand('copy');
   el.blur().hide();
 });
-$("#shabad").on("click", ".share .twitter", function() {
-  let tweet = $(this).parents(".line").children("textarea").val();
-  let shortURL = '\n'+shortenURL();
+
+$('#shabad').on('click', '.share .twitter', function () {
+  let tweet = $(this).parents('.line').children('textarea').val();
+  const shortURL = `\n${shortenURL()}`;
   if (tweet.length + shortURL.length > 134) {
-    tweet = tweet.substring(0,132-shortURL.length) + "..";
+    tweet = `${tweet.substring(0, 132 - shortURL.length)}..`;
   }
-  tweet += shortURL
-  tweet += " #sttm";
-  window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent(tweet), "_blank");
+  tweet += shortURL;
+  tweet += ' #sttm';
+  window.open(`https://twitter.com/intent/tweet?text=${encodeURIComponent(tweet)}`, '_blank');
 });
 
 /*$("#shabad").on("click", ".share .facebook", function() {
@@ -89,12 +126,12 @@ function updateSearchLang(e) {
     case 3:
     case 4:
     case 5:
-      $search.classList.remove("gurbani-font");
-      $search.placeholder = searchType === 5 ? "Ang Number" : "Khoj";
+      $search.classList.remove('gurbani-font');
+      $search.placeholder = searchType === 5 ? 'Ang Number' : 'Khoj';
       break;
     default:
-      $search.classList.add("gurbani-font");
-      $search.placeholder = "Koj";
+      $search.classList.add('gurbani-font');
+      $search.placeholder = 'Koj';
       break;
   }
   $searchType.value = searchType;
@@ -108,7 +145,7 @@ function updateSearchAction(e) {
   switch (searchType) {
     case 5:
       $form.setAttribute('action', '/ang');
-      $search.setAttribute('name','ang');
+      $search.setAttribute('name', 'ang');
       $search.removeAttribute('pattern');
       $search.removeAttribute('title', '');
       break;
@@ -116,7 +153,7 @@ function updateSearchAction(e) {
       $search.setAttribute('pattern', '.{3,}');
       $search.setAttribute('title', 'Enter 3 characters minimum.');
       $form.setAttribute('action', '/search');
-      $search.setAttribute('name','q');
+      $search.setAttribute('name', 'q');
       break;
   }
   $searchType.value = searchType;
@@ -124,82 +161,52 @@ function updateSearchAction(e) {
 
 function shabadToggle(e) {
   e.currentTarget.classList.toggle('active');
-  let option = e.currentTarget.id;
-  switch(option) {
-    case "display-options-toggle":
-      $("#display-options").toggleClass("hidden");
+  const option = e.currentTarget.id;
+  switch (option) {
+    case 'display-options-toggle':
+      document.querySelector('#display-options').classList.toggle('hidden');
       break;
-    case "unicode-toggle":
-    case "larivaar-toggle":
-    case "larivaar_assist-toggle":
-      let toggle = e.target.id.split("-")[0];
-      $(".display").toggleClass(toggle);
+    case 'unicode-toggle':
+    case 'larivaar-toggle':
+    case 'larivaar_assist-toggle': {
+      const [toggle] = e.target.id.split('-');
+      document.querySelector('.display').classList.toggle(toggle);
       checkboxPref(e, 'shabadToggles', option);
       break;
+    }
   }
 }
+
 function displayOptionToggle(e) {
+  const option = e.target.id;
+  const $display = document.querySelector('.display');
+
   e.target.classList.toggle('active');
-  let option = e.target.id;
-  $(".display").toggleClass(option);
+  $display.classList.toggle(option);
+
   checkboxPref(e, 'displayOptions', option);
 
-  //Update the textarea for copy/social sharing
-  $(".display .line").each(function() {
-    let line_share_text = [];
-    $(this).children("p:visible, blockquote:visible").each(function() {
-      let text = '';
-      if ($(this).children('div.unicode').length > 0) {
-        text = $(this).children('div.unicode').text();
-      } else {
-        text = $(this).text();
-      }
-      if (text) {
-        line_share_text.push(text);
-      }
-    });
-    $(this).find("textarea").val(line_share_text.join("\n"));
-  });
-}
+  // Update the textarea for copy/social sharing
+  [...document.querySelectorAll('.display .line')]
+    .forEach(el => el && (
+      // for every textarea
+      el.querySelector('textarea').value = (
 
-function getPrefs() {
-  $.each(default_prefs, function(key, defaults) {
-    prefs[key] = window.localStorage[key] ? JSON.parse(window.localStorage[key]) : defaults;
-  });
-}
-function setPref(key, val) {
-  window.localStorage[key] = JSON.stringify(val);
-}
-function checkboxPref(e, key, option) {
-  if (e.target.classList.contains('active')) {
-    if (prefs[key].indexOf(option) < 0) {
-      prefs[key].push(option);
-    }
-  } else {
-    let index = prefs[key].indexOf(option);
-    prefs[key].splice(index, 1);
-  }
-  setPref(key, prefs[key]);
-}
+        // get all div, blockquotes 
+        [...el.querySelector('div, blockquote')]
 
-function shortenURL(url = window.location.href) {
-  var path = window.location.pathname;
-  var shortdomain = 'sttm.ws';
+          // filter hidden ones
+          .filter(c => c.style.visibility !== 'hidden' && c.style.display !== 'none')
 
-  var r = 'http://'+shortdomain;
+          // get innerText 
+          .map(child => child.querySelectorAll('div.unicode')
+            ? child.querySelector('div.unicode').innerText
+            : el.innerText)
 
-  switch(path) {
-    case '/shabad':
-      return r+'/s/'+getParameterByName('id');
-      break;
-    case '/ang':
-      return r+'/a/'+getParameterByName('ang');
-      break;
-    case '/hukamnama':
-      return r+'/h';
-      break;
-    default:
-      return url.replace(window.location.hostname,shortdomain);
-      break;
-  }
+          // filter empty strings
+          .filter(text => text)
+
+          // join them by new line
+          .join('\n')
+      )));
 }
