31c8c484f7b2cc8254a5f2d80f05c9d793c92d13
Manjot Singh <manjot@manjotsingh.net>
2018-01-26 07:34:35 -0500

    fix #133 - search results now persist prefs on load more

--- src/js/search.js
+++ src/js/search.js
@@ -57,10 +57,12 @@ function loadResults(offset) {
         }
       }
 
-      [...prefs.displayOptions, ...prefs.shabadToggles]
-        .forEach(option => document.getElementById(option).click())
+      if (typeof offset === 'undefined') {
+        [...prefs.displayOptions, ...prefs.shabadToggles]
+          .forEach(option => document.getElementById(option).click())
 
-      $controls.classList.remove('hidden');
+        $controls.classList.remove('hidden');
+      }
     },
     error: showError
   });
