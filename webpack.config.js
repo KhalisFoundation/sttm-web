80f9598cab34bdce0dfc938b9bf4f13b5f941834
Divjot Singh <bogas04@gmail.com>
2018-03-26 23:51:15 +0530

    Fix API_URL for prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const path = require('path');
 
 const API_URLS = {
-  PRODUCTION: 'https://api.banidb.org',
+  PRODUCTION: 'https://api.banidb.org/',
   DEVELOPMENT: 'http://devapi.khajana.org/',
 };
 
@@ -11,30 +11,30 @@ const PRODUCTION = process.env.NODE_ENV === 'production';
 
 const plugins = PRODUCTION
   ? [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-      PRODUCTION: JSON.stringify(true),
-      API_URL: JSON.stringify(API_URLS.PRODUCTION),
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: 3,
-    }),
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    new UglifyJsPlugin(),
-  ]
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production'),
+        },
+        PRODUCTION: JSON.stringify(true),
+        API_URL: JSON.stringify(API_URLS.PRODUCTION),
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        minChunks: 3,
+      }),
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new UglifyJsPlugin(),
+    ]
   : [
-    new webpack.DefinePlugin({
-      PRODUCTION: JSON.stringify(false),
-      API_URL: JSON.stringify(API_URLS.DEVELOPMENT),
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: 3,
-    }),
-  ];
+      new webpack.DefinePlugin({
+        PRODUCTION: JSON.stringify(false),
+        API_URL: JSON.stringify(API_URLS.DEVELOPMENT),
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        minChunks: 3,
+      }),
+    ];
 
 const vendor = [
   'react',
@@ -63,8 +63,8 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
+        loader: 'babel-loader',
+      },
+    ],
+  },
 };
