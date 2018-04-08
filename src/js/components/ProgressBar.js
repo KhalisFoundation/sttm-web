e897115343197220a41e076da9f3f91b061856c4
Divjot Singh <bogas04@gmail.com>
2018-04-08 21:46:28 +0530

    Fix #331 (#399)
    * Initial Commit
    
    * Add default prop
    
    * Optimizations

---
+++ src/js/components/ProgressBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class ProgressBar extends React.PureComponent {
+  static defaultProps = {
+    percent: 0,
+  };
+  static propTypes = {
+    percent: PropTypes.number,
+  };
+
+  render() {
+    return (
+      <div id="progressbar-root">
+        <div
+          className="progressbar"
+          style={{
+            transform: `scaleX(${this.props.percent})`,
+            transformOrigin: '0 0',
+          }}
+        />
+      </div>
+    );
+  }
+}
