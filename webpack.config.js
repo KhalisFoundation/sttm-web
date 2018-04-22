e0c132a7d9bcf6c1693c8e7f9a7ffc90ec482233
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2018-04-22 20:09:28 +0530

    New config as per v4

--- webpack.config.js
+++ webpack.config.js
@@ -19,11 +19,6 @@ const plugins = PRODUCTION
         PRODUCTION: JSON.stringify(true),
         API_URL: JSON.stringify(API_URLS.PRODUCTION),
       }),
-      new webpack.optimize.CommonsChunkPlugin({
-        name: 'vendor',
-        minChunks: 3,
-      }),
-      new webpack.optimize.ModuleConcatenationPlugin(),
       new UglifyJsPlugin(),
     ]
   : [
@@ -31,26 +26,13 @@ const plugins = PRODUCTION
         PRODUCTION: JSON.stringify(false),
         API_URL: JSON.stringify(API_URLS.DEVELOPMENT),
       }),
-      new webpack.optimize.CommonsChunkPlugin({
-        name: 'vendor',
-        minChunks: 3,
-      }),
     ];
-
-const vendor = [
-  'react',
-  'react-dom',
-  'shabados',
-  'redux',
-  'react-redux',
-  'react-router-dom',
-];
 
 const app = path.resolve(__dirname, 'src', 'js', 'index.js');
 
 module.exports = {
+  mode: PRODUCTION || 'development',
   entry: {
-    vendor,
     app,
   },
   output: {
@@ -60,6 +42,10 @@ module.exports = {
     publicPath: 'assets/js/',
   },
   plugins,
+  optimization: {
+    noEmitOnErrors: true, // NoEmitOnErrorsPlugin
+    concatenateModules: true //ModuleConcatenationPlugin
+  },
   module: {
     rules: [
       {
