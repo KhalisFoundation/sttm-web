1f4669b1ad2ec6abf033d12ea0854c8144514b71
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2018-05-19 17:10:10 +0530

    Fix: combination of matras, half char, bindi tippi

--- src/js/components/Larivaar.js
+++ src/js/components/Larivaar.js
@@ -43,21 +43,146 @@ export default class Larivaar extends React.PureComponent {
                     // Check for unicode would be diffrent as there is no left side matra in Unicode - Unicode have right side matras only
                     // and combination of Matras + Bindi + Tippi + Half character is diffrent than fnts we use
                     // 
-                    if(splitArr[i+1] && ((splitArr[i+1].indexOf("Y")  != -1) || (splitArr[i+1].indexOf("y")  != -1) || (splitArr[i+1].indexOf("w")  != -1) || (splitArr[i+1].indexOf("I")  != -1)  || (splitArr[i+1].indexOf("u")  != -1) || (splitArr[i+1].indexOf("U")  != -1) || (splitArr[i+1].indexOf("N")  != -1) || (splitArr[i+1].indexOf("N")  != -1))) {
-                      breakSupportedCharList.push(
-                        <span key={i} style={{ color: i % 2 === 1 ? larivaarAssistColor : '' }}>
-                          {splitArr[i] + splitArr[i+1]}<wbr />
-                        </span>
-                      )
-                      i++;
-                    } else if(splitArr[i].indexOf("i") != -1) {
-                      // checking for sihari matra 
-                      breakSupportedCharList.push(
-                        <span key={i} style={{ color: i % 2 === 1 ? larivaarAssistColor : '' }}>
-                          {splitArr[i] + splitArr[i+1]}<wbr />
-                        </span>
-                      )
-                      i++;
+                    // Pairri rarra is represented by "®" in gurbani font 
+
+                    // Facepalm
+                    // 1. other form of Tippi - µ
+                    // 2. issue: there are two types of Pairri Rarra - One "R" and other "®"
+                    // 
+                    // thats why I hate punjabi fonts - No consistency
+                    // 
+                    // Done:
+                    // Kanna Bindi - single entity
+                    // Many complex words are present which contains combination of Right Side matras with Half characters and Bindi, Tippi, Adhak
+                    // and combination of Matras + Bindi + Tippi + Half character is diffrent than fonts we use
+                    // Sihari + char + Half char + Bindi/Tippi
+                    if(splitArr[i+1] && ((splitArr[i+1].indexOf("Y")  != -1) || (splitArr[i+1].indexOf("y")  != -1) || (splitArr[i+1].indexOf("w")  != -1) || (splitArr[i+1].indexOf("W")  != -1) || (splitArr[i+1].indexOf("I")  != -1)  || (splitArr[i+1].indexOf("u")  != -1) || (splitArr[i+1].indexOf("U")  != -1) || (splitArr[i+1].indexOf("N")  != -1) || (splitArr[i+1].indexOf("M")  != -1) || (splitArr[i+1].indexOf("´")  != -1) || (splitArr[i+1].indexOf("R")  != -1) || (splitArr[i+1].indexOf("Í")  != -1) || (splitArr[i+1].indexOf("µ")  != -1) || (splitArr[i+1].indexOf("@")  != -1))) {
+                      console.log("if", splitArr, splitArr[i], splitArr[i+1])
+                      // Dut akhar(Pairi Rarra) + Bindi or Tippi as one entity
+                      if(splitArr[i+1].indexOf("´")  != -1 && splitArr[i+2] && ((splitArr[i+2].indexOf("M")  != -1) || (splitArr[i+2].indexOf("N")  != -1))) {
+                        console.log('1')
+                        breakSupportedCharList.push(
+                          <span key={i} className = "wordBreakSpecialChar" style={{ color: i % 2 === 1 ? larivaarAssistColor : ''}}>
+                            {splitArr[i] + splitArr[i+1] + splitArr[i+2]}<wbr />
+                          </span>
+                        )
+                        i=i+2;
+                      } else if(splitArr[i+1].indexOf("R")  != -1 && splitArr[i+2] && ((splitArr[i+2].indexOf("w")  != -1) || (splitArr[i+1].indexOf("W")  != -1))) {
+                        console.log('2')
+                        // pairri Rarra + Kanna
+                        breakSupportedCharList.push(
+                          <span key={i} className = "wordBreakSpecialChar" style={{ color: i % 2 === 1 ? larivaarAssistColor : ''}}>
+                            {splitArr[i] + splitArr[i+1] + splitArr[i+2]}<wbr />
+                          </span>
+                        )
+                        i=i+2;
+                      } else if(splitArr[i+1].indexOf("´")  != -1 && splitArr[i+2] && ((splitArr[i+2].indexOf("Y")  != -1) || (splitArr[i+2].indexOf("y")  != -1) || (splitArr[i+2].indexOf("w")  != -1) || (splitArr[i+2].indexOf("I")  != -1)  || (splitArr[i+2].indexOf("u")  != -1) || (splitArr[i+2].indexOf("U")  != -1) || (splitArr[i+2].indexOf("N")  != -1) || (splitArr[i+2].indexOf("M")  != -1) || (splitArr[i+1].indexOf("µ")  != -1))) {
+                        console.log('3')
+                        breakSupportedCharList.push(
+                          <span key={i} className = "wordBreakSpecialChar" style={{ color: i % 2 === 1 ? larivaarAssistColor : ''}}>
+                            {splitArr[i] + splitArr[i+1] + splitArr[i+2]}<wbr />
+                          </span>
+                        )
+                        i=i+2;
+                      } else if(splitArr[i+1].indexOf("@")  != -1 && splitArr[i+2] && ((splitArr[i+2].indexOf("Y")  != -1) || (splitArr[i+2].indexOf("y")  != -1) || (splitArr[i+2].indexOf("w")  != -1) || (splitArr[i+2].indexOf("I")  != -1)  || (splitArr[i+2].indexOf("u")  != -1) || (splitArr[i+2].indexOf("U")  != -1) || (splitArr[i+2].indexOf("N")  != -1) || (splitArr[i+2].indexOf("M")  != -1) || (splitArr[i+1].indexOf("µ")  != -1))) {
+                        console.log('3')
+                        breakSupportedCharList.push(
+                          <span key={i} className = "wordBreakSpecialChar" style={{ color: i % 2 === 1 ? larivaarAssistColor : ''}}>
+                            {splitArr[i] + splitArr[i+1] + splitArr[i+2]}<wbr />
+                          </span>
+                        )
+                        i=i+2;
+                      } else if((splitArr[i+1].indexOf("´")  != -1) && splitArr[i+2] && ((splitArr[i+1].indexOf("M")  != -1) || (splitArr[i+1].indexOf("N")  != -1) || (splitArr[i+1].indexOf("µ")  != -1))) {
+                        console.log('4', splitArr[i+1])
+                        // right side matra + Bindi + Tippi
+                        breakSupportedCharList.push(
+                          <span key={i} className = "wordBreakSpecialChar" style={{ color: i % 2 === 1 ? larivaarAssistColor : ''}}>
+                            {splitArr[i] + splitArr[i+1] + splitArr[i+2]}<wbr />
+                          </span>
+                        )
+                        i=i+2;
+                      } else if((splitArr[i+1].indexOf("M")  != -1) || (splitArr[i+1].indexOf("N")  != -1)  || (splitArr[i+1].indexOf("µ")  != -1)) {
+                        console.log('5')
+                        console.log("Meu find", splitArr[i], splitArr[i+1])
+                        // right side matra + Bindi + Tippi
+                        breakSupportedCharList.push(
+                          <span key={i} style={{ color: i % 2 === 1 ? larivaarAssistColor : ''}}>
+                            {splitArr[i] + splitArr[i+1]}<wbr />
+                          </span>
+                        )
+                        i=i+1;
+                      } else if(splitArr[i+1].indexOf("´")  != -1) {
+                        breakSupportedCharList.push(
+                          <span key={i} className = "wordBreakSpecialChar" style={{ color: i % 2 === 1 ? larivaarAssistColor : ''}}>
+                            {splitArr[i] + splitArr[i+1]}<wbr />
+                          </span>
+                        )
+                        i++;
+                      } else {
+                        breakSupportedCharList.push(
+                          <span key={i} style={{ color: i % 2 === 1 ? larivaarAssistColor : '' }}>
+                            {splitArr[i] + splitArr[i+1]}<wbr />
+                          </span>
+                        )
+                        i++;  
+                      }
+
+                    } else if(splitArr[i].indexOf("i") != -1 ) {
+                      // handle bindi, tippi, dutt akhar with sihari 3 characters together
+                      // pending:
+                      // handle bindi, tippi, dutt akhar with sihari 4 character together
+                      console.log("splitArr", splitArr.join(""), splitArr[i+2], splitArr[i+3])
+                      if(splitArr[i+2] && splitArr[i+3] && (splitArr[i+2].indexOf("´")  != -1) && ((splitArr[i+3].indexOf("N")  != -1) || splitArr[i+3].indexOf("M")  != -1)) {
+                        // checking for sihari matra with pairri Yayya and Bindi or adhak 
+                        breakSupportedCharList.push(
+                          <span key={i} className = "wordBreakSpecialChar" style={{ color: i % 2 === 1 ? larivaarAssistColor : '' }}>
+                            {splitArr[i] + splitArr[i+1] + splitArr[i+2] + splitArr[i+3]}<wbr />
+                          </span>
+                        )
+                        i=i+3;
+                      } else if(splitArr[i+2] && splitArr[i+3] && (splitArr[i+2].indexOf("´")  != -1)) {
+                        // checking for sihari matra with pairri Yayya
+                        breakSupportedCharList.push(
+                          <span key={i} className = "wordBreakSpecialChar" style={{ color: i % 2 === 1 ? larivaarAssistColor : '' }}>
+                            {splitArr[i] + splitArr[i+1] + splitArr[i+2]}<wbr />
+                          </span>
+                        )
+                        i=i+2;
+                      } else if(splitArr[i+2] && splitArr[i+3] && (splitArr[i+2].indexOf("®")  != -1) && ((splitArr[i+3].indexOf("N")  != -1) || splitArr[i+3].indexOf("M")  != -1)) {
+                        // checking for sihari matra and pairri rarra other form - bindi, tippi 
+                        breakSupportedCharList.push(
+                          <span key={i} style={{ color: i % 2 === 1 ? larivaarAssistColor : '' }}>
+                            {splitArr[i] + splitArr[i+1] + splitArr[i+2] + splitArr[i+3]}<wbr />
+                          </span>
+                        )
+                        i=i+3;
+                      } else if(splitArr[i+2] && (splitArr[i+2].indexOf("®")  != -1)) {
+                        // checking for sihari matra and pairri rarra only
+                        breakSupportedCharList.push(
+                          <span key={i} style={{ color: i % 2 === 1 ? larivaarAssistColor : '' }}>
+                            {splitArr[i] + splitArr[i+1] + splitArr[i+2]}<wbr />
+                          </span>
+                        )
+                        i=i+2;
+                      } else if((splitArr[i+1].indexOf("M")  != -1) || (splitArr[i+1].indexOf("N")  != -1)  || (splitArr[i+1].indexOf("µ")  != -1)) {
+                        console.log('here...', splitArr, splitArr[i], splitArr[i+1])
+                        // right side matra + Bindi + Tippi
+                        breakSupportedCharList.push(
+                          <span key={i} style={{ color: i % 2 === 1 ? larivaarAssistColor : ''}}>
+                            {splitArr[i] + splitArr[i+1]}<wbr />
+                          </span>
+                        )
+                        i=i+1;
+                      } else {
+                        console.log('elseeee...', splitArr, splitArr[i], splitArr[i+1])
+                        // checking for sihari matra 
+                        breakSupportedCharList.push(
+                          <span key={i} style={{ color: i % 2 === 1 ? larivaarAssistColor : '' }}>
+                            {splitArr[i] + splitArr[i+1]}<wbr />
+                          </span>
+                        )
+                        i++;
+                      }
                     } else {
                       // check for all muktas
                       breakSupportedCharList.push(
