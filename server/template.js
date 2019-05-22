1069948e4a062156477fc51c247afd2d700fc760
Divjot Singh <bogas04@gmail.com>
2019-05-22 22:27:40 +0530

    Initial commit

--- server/template.js
+++ server/template.js
@@ -1,16 +1,62 @@
 import marinate from 'marinate';
 import { ONLINE_COLOR } from '../common/constants';
 
+const preloadImages = [
+  '/assets/images/sttm_logo.png',
+  '/assets/images/logo-192x192.png',
+];
+const preloadImagesHTML = preloadImages
+  .map(i => `<link async rel="preload" href="${i}" as="image">`)
+  .join('\n');
+
+const preloadFonts = [
+  '/assets/fonts/AnmolLipiSG.ttf?v=1',
+  '/assets/fonts/GurbaniAkharHeavyTrue.ttf?v=1',
+];
+const preloadFontsHTML = preloadFonts
+  .map(f => `<link async rel="preload" href="${f}" as="font"></link>`)
+  .join('\n');
+
+const preloadScripts = [
+  'assets/js/chunks/Ang~Hukamnama~Search~Shabad~Sync.js',
+  'assets/js/chunks/Ang~Hukamnama~Shabad~Sync.js',
+  'assets/js/chunks/Search.js',
+  'assets/js/chunks/Shabad.js',
+];
+const preloadScriptsHTML = preloadScripts
+  .map(s => `<link async rel="preload" href="${s}" as="script">`)
+  .join('\n');
+
+const stylesheets = [
+  '/assets/css/vendor/foundation.min.css?v=6.2.4',
+  '/assets/css/bundle.css',
+];
+const stylesheetsHTML = stylesheets
+  .map(s => `<link href="${s}" rel="stylesheet" />`)
+  .join('\n');
+
+const scripts = ['/assets/js/chunks/vendor.js', '/assets/js/app.js'];
+const scriptsHTML = scripts.map(s => `<script src="${s}"></script>`).join('\n');
+
+const registerServiceWorker = `
+  navigator.serviceWorker
+    .register('service-worker.js', { scope: './' })
+    .then(reg => console.log('Registration succeeded. Scope is ' + reg.scope))
+    .catch(console.error);
+`;
+
+const unregisterServiceWorker = `
+  navigator.serviceWorker
+    .getRegistrations()
+    .then(sws => sws.forEach(s => s.unregister()))
+`;
+
 export default ({ url, bodyClass, title, description }) => marinate`
 <!DOCTYPE html>
 <html>
 <head>
   <title>${title}</title>
-  <meta name="theme-color" content="${ONLINE_COLOR}">
   <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
-
-  <meta charset="utf-8">
-  <meta name="description" content="${description}">
   <link rel="manifest" href="/manifest.json">
 
   <!-- Preconnect -->
@@ -18,20 +64,21 @@ export default ({ url, bodyClass, title, description }) => marinate`
   <link rel="preconnect" href="//api.banidb.com" crossorigin>
 
   <!-- Preload Fonts -->
-  <link async rel="preload" href="/assets/fonts/GurbaniAkharHeavyTrue.ttf?v=1" as="font">
-  <link async rel="preload" href="/assets/fonts/AnmolLipiSG.ttf?v=1" as="font">
+  ${preloadFontsHTML}
 
   <!-- Preload JavaScript -->
-  <link async rel="preload" href="assets/js/chunks/Ang~Hukamnama~Search~Shabad~Sync.js" as="script">
-  <link async rel="preload" href="assets/js/chunks/Ang~Hukamnama~Shabad~Sync.js" as="script">
-  <link async rel="preload" href="assets/js/chunks/Search.js" as="script">
-  <link async rel="preload" href="assets/js/chunks/Shabad.js" as="script">
+  ${preloadScriptsHTML}
 
   <!-- Preload Images -->
-  <link async rel="preload" href="/assets/images/sttm_logo.png" as="image">
-  <link async rel="preload" href="/assets/images/logo-192x192.png" as="image">
+  ${preloadImagesHTML}
 
-  <!-- Open Graph and Twitter -->
+  <!-- Meta Tags -->
+  <meta charset="utf-8">
+  <meta name="theme-color" content="${ONLINE_COLOR}">
+  <meta name="description" content="${description}">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Open Graph -->
   <meta property="og:type" content="website">
   <meta property="og:url" content="https://www.sikhitothemax.org${url}">
   <meta property="og:author" content="https://khalisfoundation.org/">
@@ -40,6 +87,8 @@ export default ({ url, bodyClass, title, description }) => marinate`
   <meta property="og:image" content="/assets/images/sttm_banner.png">
   <meta property="og:image:width" content="1500">
   <meta property="og:image:height" content="1000">
+
+  <!-- Twitter -->
   <meta property="twitter:card" content="summary_large_image">
   <meta property="twitter:site" content="@khalisfound">
   <meta property="twitter:creator" content="@khalisfound">
@@ -59,11 +108,8 @@ export default ({ url, bodyClass, title, description }) => marinate`
   <meta name="msapplication-config" content="ieconfig.xml">
   <meta name="application-name" content="STTM">
 
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <!-- CSS -->
-  <link rel="stylesheet" href="/assets/css/vendor/foundation.min.css?v=6.2.4">
-  <link rel="stylesheet" href="/assets/css/bundle.css">
+  ${stylesheetsHTML}
 </head>
 
 <body class="${bodyClass}">
@@ -103,6 +149,7 @@ export default ({ url, bodyClass, title, description }) => marinate`
     </div>
   </footer>
 
+  <!-- Google Analytics -->
   <script>
     var ga;
     if (document.location.hostname === 'www.sikhitothemax.org') {
@@ -122,9 +169,20 @@ export default ({ url, bodyClass, title, description }) => marinate`
   <!-- Polyfills -->
   <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=fetch,Object.entries,Array.from,Array.prototype.includes,String.prototype.startsWith,String.prototype.includes,Map,Set,requestAnimationFrame,Array.prototype.@@iterator"></script>
 
-  <script src="/assets/js/chunks/vendor.js"></script>
-  <script src="/assets/js/app.js"></script>
-</body>
+  <!-- Source Code -->
+  ${scriptsHTML}
 
+  <!-- ServiceWorker -->
+  <script>
+    if ("serviceWorker" in navigator) {
+      ${
+        process.env.kill_service_worker
+          ? unregisterServiceWorker
+          : registerServiceWorker
+      }
+    }
+  </script>
+
+</body>
 </html>
 `;
