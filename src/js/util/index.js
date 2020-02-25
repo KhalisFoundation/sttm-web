62fe05830cf573a18384a0fa7330bde28efa9670
gauravjeetsingh <gaurav.ishwerdas@gmail.com>
2020-02-25 20:29:52 +0530

    Fixes visraam bug on mobile browsers

--- src/js/util/index.js
+++ src/js/util/index.js
@@ -11,6 +11,8 @@ import {
   LOCAL_STORAGE_KEY_FOR_VISRAAMS,
   LOCAL_STORAGE_KEY_FOR_VISRAAM_SOURCE,
   LOCAL_STORAGE_KEY_FOR_VISRAAMS_STYLE,
+  DEFAULT_VISRAAM_SOURCE,
+  DEFAULT_VISRAAM_STYLE,
 } from '../constants';
 
 import { translationMap, getGurmukhiVerse } from './api/shabad';
@@ -520,7 +522,9 @@ export const addVisraamClass = () => {
   clearVisraamClass();
   document.body.classList[getBooleanFromLocalStorage(LOCAL_STORAGE_KEY_FOR_VISRAAMS) ? 'add' : 'remove'](
     VISRAAM_CONSTANTS.CLASS_NAME,
-    VISRAAM_CONSTANTS.SOURCE_CLASS(getStringFromLocalStorage(LOCAL_STORAGE_KEY_FOR_VISRAAM_SOURCE)),
-    VISRAAM_CONSTANTS.TYPE_CLASS(getStringFromLocalStorage(LOCAL_STORAGE_KEY_FOR_VISRAAMS_STYLE))
+    VISRAAM_CONSTANTS.SOURCE_CLASS(getStringFromLocalStorage(LOCAL_STORAGE_KEY_FOR_VISRAAM_SOURCE) ||
+      DEFAULT_VISRAAM_SOURCE),
+    VISRAAM_CONSTANTS.TYPE_CLASS(getStringFromLocalStorage(LOCAL_STORAGE_KEY_FOR_VISRAAMS_STYLE) ||
+      DEFAULT_VISRAAM_STYLE)
   );
 }
