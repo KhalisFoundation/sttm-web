5f2ae7a772b854ac8af3d7b441bbd1489c7df3ad
gurjit03 <gurjitmehta@gmail.com>
2020-05-22 19:23:10 +0530

    removes stale log

--- src/js/util/get-classnames-for-shabad-ratings.ts
+++ src/js/util/get-classnames-for-shabad-ratings.ts
@@ -1,5 +1,4 @@
 export const getClassnamesForShabadRatings = (avgScore: number): string => {
-  console.log(avgScore, "avg score...")
   if (avgScore >= 80) {
     return 'Excellent'
   } else if (avgScore >= 40) {
