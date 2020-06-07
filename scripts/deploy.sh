9c8b2773d19caca6efa890787909d9cfcb19d31b
Taranjit Singh <tsingh777@gmail.com>
2020-06-07 11:56:15 -0700

    Build prompts auto answer for ssh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-ssh -p $PORT $REMOTE_USER@$REMOTE_HOST_1 "deploy-$1"
-ssh -p $PORT $REMOTE_USER@$REMOTE_HOST_2 "deploy-$1"
+ssh -o StrictHostKeyChecking=accept-new -p $PORT $REMOTE_USER@$REMOTE_HOST_1 "deploy-$1"
+ssh -o StrictHostKeyChecking=accept-new -p $PORT $REMOTE_USER@$REMOTE_HOST_2 "deploy-$1"
