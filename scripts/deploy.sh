d6a28260989db360f452f2d1e7230e045454c951
Taranjit Singh <tsingh777@gmail.com>
2020-06-07 12:00:37 -0700

    change option for build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-ssh -o StrictHostKeyChecking=accept-new -p $PORT $REMOTE_USER@$REMOTE_HOST_1 "deploy-$1"
-ssh -o StrictHostKeyChecking=accept-new -p $PORT $REMOTE_USER@$REMOTE_HOST_2 "deploy-$1"
+ssh -o StrictHostKeyChecking=no -p $PORT $REMOTE_USER@$REMOTE_HOST_1 "deploy-$1"
+ssh -o StrictHostKeyChecking=no -p $PORT $REMOTE_USER@$REMOTE_HOST_2 "deploy-$1"
