00ae40c41cc755d74f0b3cd1a6273c95a35c6b9e
Gauravjeet singh <panda@ryaz.io>
2020-06-16 11:18:58 +0530

    Handles the empty doodle endpoint; handles the sttm api fetch gracefully

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -35,7 +35,10 @@ export default class Header extends React.PureComponent {
     fetch(`${DOODLE_URL}`)
       .then(r => r.json())
       .then((data) => {
-        if (data) {
+        console.log(data);
+        if (data.error) {
+          this.setState({ showDoodle: false, doodleData: null });
+        } else if (data.rows.length) {
           this.setState({ showDoodle: true, doodleData: data.rows[0] });
         }
       }, (error) => {
