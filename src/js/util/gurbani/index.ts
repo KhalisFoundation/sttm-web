568ebeb76c7a0c1a82900f86476a10ee064d564c
gurjit03 <gurjitmehta@gmail.com>
2020-07-02 16:33:01 +0530

    merges upstream dev

--- src/js/util/gurbani/index.ts
+++ src/js/util/gurbani/index.ts
@@ -1,3 +1,3 @@
-export * from './verses-to-gurbani';
-export * from './get-highlighting-endpoints';
 export * from './get-highlight-indices';
+export * from './get-highlighting-endpoints';
+export * from './verses-to-gurbani';
