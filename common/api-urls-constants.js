cb662774cdbff4bfe7d89643be4f1a3ca91ee7c8
gurjit03 <gurjitmehta@gmail.com>
2020-07-02 18:04:41 +0530

    fixes linting issue

--- common/api-urls-constants.js
+++ common/api-urls-constants.js
@@ -1,4 +1,4 @@
-const isProduction = process.env.NODE_ENV !== 'production';
+const isProduction = process.env.NODE_ENV == 'production';
 
 module.exports = {
   BANIS: isProduction ? '//api.banidb.com/v2/banis' : "//api.khajana.org/v2/banis",
