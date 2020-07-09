98a1d94504076c3b024211eb2541f6b9f417458a
gurjit03 <gurjitmehta@gmail.com>
2020-07-09 21:15:06 +0530

    removes 2 tests due to basic limitation of React Testing Library

--- src/js/components/__tests__/Meta.test.js
+++ src/js/components/__tests__/Meta.test.js
@@ -4,14 +4,12 @@ import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 import {
   render,
-  fireEvent,
   screen,
 } from '@testing-library/react';
 import { toHaveClass } from '@testing-library/jest-dom/matchers'
 expect.extend({ toHaveClass });
 
 import { shabad, mockStore } from '../__mocks__/';
-import FullScreen from '../FloatingActions/FullScreen';
 import Meta from '../Meta';
 
 //NOTE: MemoryRouter - https://testing-library.com/docs/example-react-router#reducing-boilerplate
@@ -27,15 +25,12 @@ describe('<Meta />', () => {
     beforeEach(() => {
       renderedComponent = render(
         <Provider store={store}>
-          <div>
-            <FullScreen />
-            <Meta
-              type="ang"
-              isUnicode={false}
-              info={shabad.shabadInfo}
-              translationLanguages={translationLanguages}
-              transliterationLanguages={transliterationLanguages} />
-          </div>
+          <Meta
+            type="ang"
+            isUnicode={false}
+            info={shabad.shabadInfo}
+            translationLanguages={translationLanguages}
+            transliterationLanguages={transliterationLanguages} />
         </Provider >,
         { wrapper: MemoryRouter }
       );
@@ -46,15 +41,15 @@ describe('<Meta />', () => {
       expect(container).toMatchSnapshot();
     })
 
-    it('shows navigation arrows properly on fullscreen mode', async () => {
-      const { container } = renderedComponent;
-      const fullscreen = container.querySelector('.fullscreen');
-      await fireEvent.click(fullscreen);
+    // it('shows navigation arrows properly on fullscreen mode', async () => {
+    //   const { container } = renderedComponent;
+    //   const fullscreen = container.querySelector('.fullscreen');
+    //   await fireEvent.click(fullscreen);
 
-      // Basic limitation with css style as we can't test style changes on nav items
-      const body = document.querySelector('body');
-      expect(body).toHaveClass('fullscreen-view');
-    });
+    //   // Basic limitation with css style as we can't test style changes on nav items
+    //   const body = document.querySelector('body');
+    //   expect(body).toHaveClass('fullscreen-view');
+    // });
   });
 
   describe('<Meta type=\"shabad\" />', () => {
@@ -87,15 +82,12 @@ describe('<Meta />', () => {
     beforeEach(() => {
       renderedComponent = render(
         <Provider store={store}>
-          <div>
-            <FullScreen />
-            <Meta
-              type="hukamnama"
-              isUnicode={false}
-              info={shabad.shabadInfo}
-              translationLanguages={translationLanguages}
-              transliterationLanguages={transliterationLanguages} />
-          </div>
+          <Meta
+            type="hukamnama"
+            isUnicode={false}
+            info={shabad.shabadInfo}
+            translationLanguages={translationLanguages}
+            transliterationLanguages={transliterationLanguages} />
         </Provider >,
         { wrapper: MemoryRouter }
       );
@@ -108,16 +100,6 @@ describe('<Meta />', () => {
     it('shows hours navigation buttons', () => {
       const { queryAllByTestId } = renderedComponent;
       expect(screen.queryAllByTestId('hour24')).toHaveLength(2);
-    });
-
-    it('dont show navigation arrows on fullscreen mode', async () => {
-      const { container } = renderedComponent;
-      const fullscreen = container.querySelector('.fullscreen');
-      await fireEvent.click(fullscreen);
-
-      // Basic limitation with css style as we can't test style changes on nav items
-      const body = document.querySelector('body');
-      expect(body).toHaveClass('fullscreen-view');
     });
   });
 });
