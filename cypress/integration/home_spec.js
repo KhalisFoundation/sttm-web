3457ad228c527da01b0469e4621e677d12388302
plug-n-play <>
2020-08-12 22:27:04 +0530

    clean-up

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -2,18 +2,6 @@ describe('Home page Search tests', () => {
   it('should search shabad Jo Mange Thakur Apne Te with default settings', () => {
     cy.visit('/')
 
-    // search using first letters
-    // cy.get('#search')
-    //   .type('jmTAq')
-    //   .should('have.value', 'jmTAq')
-
-    // cy.get('.search-result')
-    //   .should('contain', 'jo')
-    //   .should('contain', 'mwgih')
-    //   .should('contain', 'Twkur')
-    //   .should('contain', 'Apuny')
-    //   .should('contain', 'qy')
-
     cy.get('#search')
       .clear()
       .type('bhb')
