4ef4cdf5e3503fe47bcbb5bbaa32cab4735fc2a3
gurjit03 <gurjitmehta@gmail.com>
2020-08-27 20:30:36 +0530

    adds 4 tests for sundar-gutka page

--- cypress/integration/navigation/sundar-gutka.spec.js
+++ cypress/integration/navigation/sundar-gutka.spec.js
@@ -1,3 +1,5 @@
+import { getRandomNumber } from '../../utils';
+import { sanitizeBaani } from '../../../src/js/pages/SundarGutka/utils';
 const sttmBlue = 'rgb(1, 102, 155)';
 
 Cypress.Commands.add('checkSgCard', function checkGranthIndices({ childNo, english, gurmukhi }) {
@@ -23,6 +25,7 @@ Cypress.Commands.add('checkSgCard', function checkGranthIndices({ childNo, engli
 
 describe('Navigation', () => {
   const sundarGutkaBaanis = 104;
+  const searchString = 'jaa';
 
   context('Sundar-Gutka page', () => {
     beforeEach(() => {
@@ -46,14 +49,52 @@ describe('Navigation', () => {
       cy.checkSgCard({ childNo: sundarGutkaBaanis - 1, english: 'kaanaRe kee vaar mahalaa chauthhaa', gurmukhi: 'ਕਾਨੜੇ ਕੀ ਵਾਰ ਮਹਲਾ ੪' })
     })
 
-    it.only('should open correct bani on clicking sundar gutka card', () => {
+    it('should open correct bani on clicking sundar gutka card', () => {
 
       cy.get('.sgCard')
         .first()
         .click()
 
       cy.url().should('contain', '/sundar-gutka/gur-mantr');
-
     })
 
+    it.only(`should display appriopriate number of cards on searching with string ${searchString}`, () => {
+      cy.get('.search')
+        .type(searchString)
+
+      cy.get('.sgCard')
+        .should('have.length', 7)
+        .each((sgCard, idx) => {
+          const href = sgCard.parent().attr('href');
+          expect(href).to.include(searchString);
+        });
+    })
+
+    it('should generate user friendly url', () => {
+      cy.server();
+
+      cy.route({ method: "GET", url: 'http://api.khajana.org/v2/banis' })
+        .as('sgBanis')
+
+      cy.wait('@sgBanis').then(banis => {
+        const banisData = banis.response.body;
+        const baniLinks = banisData.map(({ transliteration }) => `/sundar-gutka/${sanitizeBaani(transliteration).split(' ').join('-')}`);
+        const randomIdx = getRandomNumber(baniLinks.length);
+        const lastIdx = baniLinks.length - 1;
+
+        cy.get('.sgCard').each((sgCard, idx) => {
+          // we gonna check just 3 random indexes
+          if (idx === 0 ||
+            idx === randomIdx ||
+            idx === lastIdx) {
+            const href = sgCard.parent().attr('href');
+            expect(href).to.equal(baniLinks[idx])
+          }
+        });
+      })
+    })
+
+    it('should work correctly using controls from settings panel', () => {
+
+    })
   })
---
+++ cypress/utils/get-random-number.js
@@ -0,0 +1,1 @@
+export const getRandomNumber = (max) => Math.floor(Math.random() * Math.floor(max));
