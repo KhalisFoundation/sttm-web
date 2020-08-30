34ca038001b75877f9a690419361ffab13758c8c
gurjit03 <gurjitmehta@gmail.com>
2020-08-30 13:35:28 +0530

    adds 2 tests for full-word(G), and creates cypress env variable colors

--- cypress/integration/search/full-word-gurmukhi.spec.js
+++ cypress/integration/search/full-word-gurmukhi.spec.js
@@ -1,12 +1,12 @@
 import { SEARCH_TYPES } from '../../../src/js/constants';
-const searchType = SEARCH_TYPES.ROMANIZED_FIRST_LETTERS_ANYWHERE.toString();
+const searchType = SEARCH_TYPES.GURMUKHI_WORD.toString();
 
 describe('Search', () => {
-  const searchString = 'jmtatssd';
-  const searchString2 = 'jmta';
+  const searchString = 'sMqoK';
   const showAllResultsBtn = 'Show full results';
+  const { sttmBlue } = Cypress.env().colors;
 
-  describe('Romanized First Letter Search anywhere(E)', () => {
+  describe('Full Word(G)', () => {
     beforeEach(() => {
       cy.visit('/');
 
@@ -18,22 +18,27 @@ describe('Search', () => {
     });
 
     it(`should return search results for ${searchString} in drop-down`, () => {
-
       cy.get('#search')
         .type(searchString);
 
       cy.get('.search-result span')
-        .should('contain', 'jo')
-        .should('contain', 'mwgih')
-        .should('contain', 'Twkur')
-        .should('contain', 'Apuny')
-        .should('contain', 'qy')
-        .should('contain', 'soeI')
-        .should('contain', 'soeI')
-        .should('contain', 'dyvY')
+        .should('contain', ' sMqoKI') //Santokhi
+        .should('contain', ' sMqoKu') //Santokhu
+        .should('contain', 'sMqoK') //Santokh
     });
 
-    it(`should contain ${searchString2} in first letters of the words [in results]`, () => {
+    it(`should return highlight search result words for  ${searchString} in drop-down`, () => {
+      cy.get('#search')
+        .type(searchString);
+
+      cy.get('.search-highlight-word')
+        .should('contain', ' sMqoKI') //Santokhi
+        .should('contain', ' sMqoKu') //Santokhu
+        .should('contain', 'sMqoK') //Santokh
+        .should('have.css', 'color', sttmBlue)
+    })
+
+    it.only(`should contain ${searchString2} in first letters of the words [in results]`, () => {
 
       cy.get('#search')
         .type(searchString2);
@@ -53,22 +58,22 @@ describe('Search', () => {
       });
     })
 
-    it(`should open all search results page on clicking the ${showAllResultsBtn} button`, () => {
+    //   it(`should open all search results page on clicking the ${showAllResultsBtn} button`, () => {
 
-      cy.get('#search')
-        .type(searchString2)
+    //     cy.get('#search')
+    //       .type(searchString2)
 
-      cy.get('#suggestions')
-        .as('suggestions')
+    //     cy.get('#suggestions')
+    //       .as('suggestions')
 
-      cy.get('@suggestions')
-        .scrollTo('bottom')
-        .find('.show-all-results a')
-        .should('exist')
-        .scrollIntoView()
-        .click({ force: true })
+    //     cy.get('@suggestions')
+    //       .scrollTo('bottom')
+    //       .find('.show-all-results a')
+    //       .should('exist')
+    //       .scrollIntoView()
+    //       .click({ force: true })
 
-      cy.url().should('include', `/search?q=${searchString2}&type=${searchType}&source=G`);
-    })
+    //     cy.url().should('include', `/search?q=${searchString2}&type=${searchType}&source=G`);
+    //   })
+    // })
   })
-})
