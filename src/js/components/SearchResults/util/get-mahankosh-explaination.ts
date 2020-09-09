3acd63dc253c09ec515f5788e5d2c1577715ca45
gurjit03 <gurjitmehta@gmail.com>
2020-09-10 00:39:34 +0530

    changes loading text

--- src/js/components/SearchResults/util/get-mahankosh-explaination.ts
+++ src/js/components/SearchResults/util/get-mahankosh-explaination.ts
@@ -2,7 +2,7 @@ import { IMahankoshData } from '@/types';
 
 export const getMahankoshExplaination = (mahankoshData: IMahankoshData[], isLoading: boolean) => {
   if (isLoading) {
-    return 'loading...'
+    return '....'
   }
 
   if (mahankoshData.length > 0) {
