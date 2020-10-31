c25ab41fd404279b626672b2000756ae61ca8d4e
Taranjit Singh <tsingh777@gmail.com>
2020-10-31 16:05:30 -0700

    Fix travis scripts

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
-echo "Pull Request Branch Repo=$TRAVIS_PULL_REQUEST_SLUG"
-echo "Brnach Repo=$TRAVIS_REPO_SLUG"
-echo "Is a PullRequest=$TRAVIS_PULL_REQUEST"
-if [[ $TRAVIS_PULL_REQUEST != "false" && $TRAVIS_PULL_REQUEST_SLUG != "$TRAVIS_REPO_SLUG" ]]
+if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_REPO_SLUG" == "KhalisFoundation/sttm-web" && "$TRAVIS_PULL_REQUEST_SLUG" == '' ]]
 then
     # Only run this pushes to branches in main repo and not on forks and pull requests.
     openssl aes-256-cbc -K $encrypted_1454d8bcbe1b_key -iv $encrypted_1454d8bcbe1b_iv -in deploy.enc -out scripts/deploy -d
--- scripts/set-up.sh
+++ scripts/set-up.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
-echo "Pull Request Branch Repo=$TRAVIS_PULL_REQUEST_SLUG"
-echo "Brnach Repo=$TRAVIS_REPO_SLUG"
-echo "Is a PullRequest=$TRAVIS_PULL_REQUEST"
-if [[ $TRAVIS_PULL_REQUEST != "false" && $TRAVIS_PULL_REQUEST_SLUG != "$TRAVIS_REPO_SLUG" ]]
+if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_REPO_SLUG" == "KhalisFoundation/sttm-web" && "$TRAVIS_PULL_REQUEST_SLUG" == '' ]]
 then
     # Only run this pushes to branches in main repo and not on forks and pull requests.
     chmod 600 scripts/deploy && mv scripts/deploy ~/.ssh/id_rsa
