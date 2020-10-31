5c55dfd188122c6788f196e639f146edfa83a052
Taranjit Singh <tsingh777@gmail.com>
2020-10-31 15:52:14 -0700

    Fix travis scripts

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
+echo "Pull Request Branch Repo=$TRAVIS_PULL_REQUEST_SLUG"
+echo "Brnach Repo=$TRAVIS_REPO_SLUG"
+echo "Is a PullRequest=$TRAVIS_PULL_REQUEST"
 if [[ $TRAVIS_PULL_REQUEST != "false" && $TRAVIS_PULL_REQUEST_SLUG != "$TRAVIS_REPO_SLUG" ]]
 then
     # Only run this pushes to branches in main repo and not on forks and pull requests.
-    chmod 600 scripts/deploy && mv scripts/deploy ~/.ssh/id_rsa
+    openssl aes-256-cbc -K $encrypted_1454d8bcbe1b_key -iv $encrypted_1454d8bcbe1b_iv -in deploy.enc -out scripts/deploy -d
 fi
