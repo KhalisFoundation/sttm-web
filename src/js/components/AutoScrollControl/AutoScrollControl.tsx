99ac09549f189ae8eee35ea64dcc7f673df11304
gurjit03 <gurjitmehta@gmail.com>
2021-01-15 14:05:20 +0530

    updates next scroll position

--- src/js/components/AutoScrollControl/AutoScrollControl.tsx
+++ src/js/components/AutoScrollControl/AutoScrollControl.tsx
@@ -144,14 +144,12 @@ class AutoScrollControl extends React.PureComponent<IAutoScrollControlProps, IAu
       const [scrollingSpeed] = this.state.scrollingSpeed;
 
       // We are having minimum scroll per pixel + adding the extra dynamic pixel movement based on slider value.
-      // We are having 2 different threshold for speeds controls - higher and lower
-
       const movement = toFixedFloat((0.1 + ((scrollingSpeed / 100) * AutoScrollControl.thresHold)), 3);
 
       // Only allow the scrolling if we have surpassed previous scrolls or if it's firefox browser.
       if (this._isFirefoxAgent || scrollY >= Math.round(this._nextScrollPosition)) {
         this._nextScrollPosition += movement;
-        window.scrollTo({ left: 0, top: this._nextScrollPosition, behavior: 'smooth' });
+        window.scrollTo({ left: 0, top: Math.round(this._nextScrollPosition), behavior: 'smooth' });
       }
 
       this._interval = requestAnimationFrame(this.handleAutoScroll);
