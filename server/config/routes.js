e7a8aa5b468b9b9a40d654b076dfb988206834ca
Navdeep Singh <navdeep.er@gmail.com>
2021-05-13 11:50:59 +0530

    Update

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,16 +1,16 @@
 const jwtSign = require('../utils/jwt')
 const Passport = require("passport");
 
-const getJwt = (req, res, next) => {
+const getJwt = (req, res) => {
   const token = jwtSign('navdeep.er@gmail.com');
   res.send(token)
 };
 
-const googleSignIn = (req, res, next) => {
+const googleSignIn = () => {
   Passport.authenticate('google', { scope: ['profile', 'email'] });
 }
 
-const googleSignInCallback = (req, res, next) => {
+const googleSignInCallback = () => {
   Passport.authenticate('google', { failureRedirect: '/fail' },
   function(req, res) {
     // Successful authentication, redirect home.
