4ad1a3528efa967bc579a4be463950a1ee9573d0
gurjit03 <gurjitmehta@gmail.com>
2022-05-29 20:12:41 +0530

    add preview shabad component

---
+++ src/js/components/PreviewShabad.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PreviewShabad = (props) => {
+  const { verses } = props;
+  const displayVerseCount = 15;
+  const totalVerseCount = verses.length;
+  return (
+    <div className="preview-shabad-background">
+      <div className="preview-shabad-container">
+        <div className="preview-shabad-title">PREVIEW</div>
+        <div className="preview-shabad-body">
+          <div className="shabad-verses">
+            {verses.slice(0, displayVerseCount).map((item) => (
+              <div key={item.verseId}>{item.verse.unicode}</div>
+            ))}
+          </div>
+        </div>
+        {totalVerseCount - displayVerseCount > 0 && (
+          <div className="preview-shabad-footer">
+            + {totalVerseCount - displayVerseCount} more lines...
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+PreviewShabad.propTypes = {
+  verses: PropTypes.array,
+};
+
+export default PreviewShabad;
