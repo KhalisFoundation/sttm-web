74c06b995c676eda6e472d56e726bbe837292f0c
gurjit03 <gurjitmehta@gmail.com>
2022-07-14 02:16:02 +0530

    close settings panel on pressing multiview and on pressing add or remove shabad buttons

--- src/js/components/MultiViewButton.tsx
+++ src/js/components/MultiViewButton.tsx
@@ -2,20 +2,25 @@ import React from 'react'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import cx from 'classnames';
 import { Multiview } from '@/components/Icons/Multiview';
-import { setMultiViewPanel } from "@/features/actions";
+import { setMultiViewPanel, closeSettingsPanel } from "@/features/actions";
 
 interface IMultiViewButton {
-  showMultiViewPanel: boolean
+  showMultiViewPanel: boolean,
+  showPinSettings: boolean,
 }
 
 const MultiViewButton = (props: any) => {
   const dispatch = useDispatch()
   const typedUseSelector: TypedUseSelectorHook<IMultiViewButton> = useSelector;
   const showMultiViewPanel = typedUseSelector(state => state.showMultiViewPanel)
+  const showPinSettings = typedUseSelector(state => state.showPinSettings)
 
   const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    dispatch(setMultiViewPanel(!showMultiViewPanel))
+    dispatch(setMultiViewPanel(!showMultiViewPanel));
+    if (showPinSettings) {
+      dispatch(closeSettingsPanel());
+    }
   }
 
   return (
