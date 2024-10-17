efb995860d0c6ac837ffeec44c4aa5f1b6593821
Gurjit Singh <gurjitmehta@gmail.com>
2024-10-17 17:20:03 +0200

    Fix api urls constants (#1749)
    * chore: adds production url back to the api constants
    
    * chore: updates package.json

--- common/api-urls-constants.js
+++ common/api-urls-constants.js
@@ -2,6 +2,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   BANIS: isProduction ? '//api.banidb.com/v2/banis' : "//api.khajana.org/v2/banis",
+  PRODUCTION: '//api.banidb.com/v2/',
   DEVELOPMENT: '//api.khajana.org/v2/',
   AMRIT_KEERTAN: '//api.banidb.com/v2/amritkeertan',
   AMRIT_KEERTAN_SHABADS: '//api.banidb.com/v2/shabads',
