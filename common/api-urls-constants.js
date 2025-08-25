77638ddbe9a1ae2d3787637aa98bb6b525685136
Gauravjeet Singh <gaurav.ishwerdas@gmail.com>
2025-08-25 15:22:17 +0530

    fix: revert change in API Urls

--- common/api-urls-constants.js
+++ common/api-urls-constants.js
@@ -2,12 +2,12 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   BANIS: isProduction
-    ? '//localhost:3000/v2/banis'
-    : '//localhost:3000/v2/banis',
-  PRODUCTION: '//localhost:3000/v2/',
-  DEVELOPMENT: '//localhost:3000/v2/',
-  AMRIT_KEERTAN: '//localhost:3000/v2/amritkeertan',
-  AMRIT_KEERTAN_SHABADS: '//localhost:3000/v2/shabads',
+    ? '//api.banidb.com/v2/banis'
+    : '//api.khajana.org/v2/banis',
+  PRODUCTION: '//api.banidb.com/v2/',
+  DEVELOPMENT: '//api.khajana.org/v2/',
+  AMRIT_KEERTAN: '//api.banidb.com/v2/amritkeertan',
+  AMRIT_KEERTAN_SHABADS: '//api.banidb.com/v2/shabads',
   BANNERS: '//api.sikhitothemax.org/messages/web',
   SYNC: {
     PRODUCTION: '//api.sikhitothemax.org/',
@@ -15,7 +15,7 @@ module.exports = {
   },
   CEREMONIES: '//api.sikhitothemax.org/ceremonies/',
   DOODLE: '//api.sikhitothemax.org/doodle/',
-  WRITERS: '//localhost:3000/v2/writers/',
+  WRITERS: '//api.banidb.com/v2/writers/',
   GURBANIBOT: '//gurbanichatbot.sikhitothemax.org/',
   SP_API: '//serviceprovider.khalis.net',
 };
