5c97298e5279b46af083e9638edbd63911b7514f
Gauravjeet Singh <gaurav.ishwerdas@gmail.com>
2025-12-10 17:32:43 -0800

    fix: restore the previous ui of icons

--- src/js/components/Icons/MicIcon.tsx
+++ src/js/components/Icons/MicIcon.tsx
@@ -62,9 +62,9 @@ const MicIcon: React.FC<MicIconProps> = ({
         if (e.data.size > 0) audioChunksRef.current.push(e.data);
       };
 
-      mediaRecorder.onstop = () => {
+      mediaRecorder.onstop = async () => {
         setIsProcessing(true);
-        processAudio();
+        await processAudio();
       };
 
       mediaRecorder.start();
@@ -220,7 +220,7 @@ const MicIcon: React.FC<MicIconProps> = ({
   if (isRecording) {
     return (
       <button type="button" onClick={handleClick} title="Stop recording">
-        <svg fill={fill} width={width} height={height} viewBox="0 0 24 24">
+        <svg fill={fill} width={width} height={height} viewBox="0 0 24 24" {...props}>
           <rect x="6" y="6" width="12" height="12" rx="2" />
         </svg>
       </button>
@@ -228,11 +228,19 @@ const MicIcon: React.FC<MicIconProps> = ({
   }
 
   return (
-    <button type="button" onClick={handleClick} title="Start recording">
-      <svg fill={fill} width={width} height={height} viewBox="0 0 24 24">
-        <path d="M12 14c1.66 0 3-1.34 3-3V5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3z" />
-        <path d="M17 11c0 2.76-2.24 5-5 5s-5-2.24-5-5H5c0 3.53 2.61 6.43 6 6.92V21h2v-3.08c3.39-.49 6-3.39 6-6.92h-2z" />
-      </svg>
+    <button type="button" onClick={handleClick} title="Start voice recording">
+      <div className="mic-icon-container">
+        <svg
+          fill={fill}
+          width={width}
+          height={height}
+          viewBox="0 0 24 24"
+          {...props}
+        >
+          <path d="M12 14c1.66 0 3-1.34 3-3V5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3z" />
+          <path d="M17 11c0 2.76-2.24 5-5 5s-5-2.24-5-5H5c0 3.53 2.61 6.43 6 6.92V21h2v-3.08c3.39-.49 6-3.39 6-6.92h-2z" />
+        </svg>
+      </div>
     </button>
   );
 };
